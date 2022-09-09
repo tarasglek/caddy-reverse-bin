3f00051d4bf3770f6ce75a446bb3141e033c49c1
Andreas Schneider <aksdb@gmx.de>
2022-09-09 22:23:10 +0200

    📝 Update documentation

--- doc.go
+++ doc.go
@@ -5,8 +5,7 @@ modern, full-featured, easy-to-use web server.
 It has been forked from the fantastic work of Kurt Jung who wrote that
 plugin for Caddy 1.
 
-
-Documentation
+# Documentation
 
 This plugin lets you generate dynamic content on your website by means
 of command line scripts. To collect information about the inbound HTTP
@@ -40,19 +39,19 @@ that scripts will be run with the same permissions as Caddy itself. This
 can sometimes be less than ideal, for example when your script needs to
 read or write files associated with a different owner.
 
-Security Considerations
+# Security Considerations
 
 Serving dynamic content exposes your server to more potential threats
 than serving static pages. There are a number of considerations of which
 you should be aware when using CGI applications.
 
-CGI SCRIPTS SHOULD BE LOCATED OUTSIDE OF CADDY'S DOCUMENT ROOT.
+CGI scripts should be located outside of Caddy's document root.
 Otherwise, an inadvertent misconfiguration could result in Caddy
 delivering the script as an ordinary static resource. At best, this
 could merely confuse the site visitor. At worst, it could expose
 sensitive internal information that should not leave the server.
 
-MISTRUST THE CONTENTS OF PATH_INFO, QUERY_STRING AND STANDARD INPUT.
+Mistrust the contents of PATH_INFO, QUERY_STRING and standard input.
 Most of the environment variables available to your CGI program are
 inherently safe because they originate with Caddy and cannot be modified
 by external users. This is not the case with PATH_INFO, QUERY_STRING
@@ -61,12 +60,12 @@ sure to validate and sanitize all inbound content. If you use a CGI
 library or framework to process your scripts, make sure you understand
 its limitations.
 
-Errors
+# Errors
 
 An error in a CGI application is generally handled within the
 application itself and reported in the headers it returns.
 
-Application Modes
+# Application Modes
 
 Your CGI application can be executed directly or indirectly. In the
 direct case, the application can be a compiled native executable or it
@@ -80,22 +79,20 @@ setting up the filename extension association.
 In the indirect case, the name of the CGI script is passed to an
 interpreter such as lua, perl or python.
 
-Requirements
-
+# Requirements
 
 -   This module needs to be installed (obviously).
 
-    Refer to the Caddy documentation on how to build Caddy with
-    plugins/modules.
-
+	Refer to the Caddy documentation on how to build Caddy with
+	plugins/modules.
 
 -   The directive needs to be registered in the Caddyfile:
 
-        {
-            order cgi last
-        }
+	{
+	    order cgi last
+	}
 
-Basic Syntax
+# Basic Syntax
 
 The basic cgi directive lets you add a handler in the current caddy
 router location with a given script and optional arguments. The matcher
@@ -103,11 +100,11 @@ is a default caddy matcher that is used to restrict the scope of this
 directive. The directive can be repeated any reasonable number of times.
 Here is the basic syntax:
 
-    cgi [matcher] exec [args...]
+	cgi [matcher] exec [args...]
 
 For example:
 
-    cgi /report /usr/local/cgi-bin/report
+	cgi /report /usr/local/cgi-bin/report
 
 When a request such as https://example.com/report or
 https://example.com/report/weekly arrives, the cgi middleware will
@@ -117,14 +114,14 @@ is assumed that the script is self-contained, for example a pre-compiled
 CGI application or a shell script. Here is an example of a standalone
 script, similar to one used in the cgi plugin's test suite:
 
-    #!/bin/bash
+	#!/bin/bash
 
-    printf "Content-type: text/plain\n\n"
+	printf "Content-type: text/plain\n\n"
 
-    printf "PATH_INFO    [%s]\n" $PATH_INFO
-    printf "QUERY_STRING [%s]\n" $QUERY_STRING
+	printf "PATH_INFO    [%s]\n" $PATH_INFO
+	printf "QUERY_STRING [%s]\n" $QUERY_STRING
 
-    exit 0
+	exit 0
 
 The environment variables PATH_INFO and QUERY_STRING are populated and
 passed to the script automatically. There are a number of other standard
@@ -138,34 +135,35 @@ path left of the matcher from the full URL. Therefore if you require
 your CGI program to know the SCRIPT_NAME, make sure to pass that
 explicitly:
 
-    cgi /script.cgi* /path/to/my/script someargument {
-      script_name /script.cgi
-    }
+	cgi /script.cgi* /path/to/my/script someargument {
+	  script_name /script.cgi
+	}
 
-Advanced Syntax
+# Advanced Syntax
 
 In order to specify custom environment variables, pass along one or more
 environment variables known to Caddy, or specify more than one match
 pattern for a given rule, you will need to use the advanced directive
 syntax. That looks like this:
 
-    cgi [matcher] exec [args...] {
-        script_name subpath
-        dir working_directory
-        env key1=val1 [key2=val2...]
-        pass_env key1 [key2...]
-        pass_all_env
-        buffer_limit <size>
-        inspect
-    }
+	cgi [matcher] exec [args...] {
+	    script_name subpath
+	    dir working_directory
+	    env key1=val1 [key2=val2...]
+	    pass_env key1 [key2...]
+	    pass_all_env
+	    buffer_limit <size>
+	    unbuffered_output
+	    inspect
+	}
 
 For example,
 
-    cgi /sample/report* /usr/local/bin/reportscript.sh {
-        script_name /sample/report
-        env DB=/usr/local/share/app/app.db SECRET=/usr/local/share/app/secret CGI_LOCAL=
-        pass_env HOME UID
-    }
+	cgi /sample/report* /usr/local/bin/reportscript.sh {
+	    script_name /sample/report
+	    env DB=/usr/local/share/app/app.db SECRET=/usr/local/share/app/secret CGI_LOCAL=
+	    pass_env HOME UID
+	}
 
 The script_name subdirective helps the cgi module to separate the path
 to the script from the (virtual) path afterwards (which shall be passed
@@ -205,7 +203,14 @@ go-humanize. Default: 4MiB.
 (An example of this is git push if the objects to push are larger than
 the http.postBuffer)
 
-Troubleshooting
+With the unbuffered_output subdirective it is possible to instruct the
+CGI handler to flush output from the CGI script as soon as possible. By
+default, the output is buffered into chunks before it is being written
+to optimize the network usage and allow to determine the Content-Length.
+When unbuffered, bytes will be written as soon as possible. This will
+also force the response to be written in chunked encoding.
+
+# Troubleshooting
 
 If you run into unexpected results with the CGI plugin, you are able to
 examine the environment in which your CGI application runs. To enter
@@ -218,43 +223,43 @@ variables to which your CGI application has access.
 
 For example, consider this example CGI block:
 
-    cgi /wapp/*.tcl /usr/local/bin/wapptclsh /home/quixote/projects{path} {
-        script_name /wapp
-        pass_env HOME LANG
-        env DB=/usr/local/share/app/app.db SECRET=/usr/local/share/app/secret
-        inspect
-    }
+	cgi /wapp/*.tcl /usr/local/bin/wapptclsh /home/quixote/projects{path} {
+	    script_name /wapp
+	    pass_env HOME LANG
+	    env DB=/usr/local/share/app/app.db SECRET=/usr/local/share/app/secret
+	    inspect
+	}
 
 When you request a matching URL, for example,
 
-        https://example.com/wapp/hello.tcl
+	https://example.com/wapp/hello.tcl
 
 the Caddy server will deliver a text page similar to the following. The
 CGI application (in this case, wapptclsh) will not be called.
 
-    CGI for Caddy inspection page
+	CGI for Caddy inspection page
 
-    Executable .................... /usr/local/bin/wapptclsh
-      Arg 1 ....................... /home/quixote/projects/hello.tcl
-    Root .......................... /
-    Dir ........................... /home/quixote/www
-    Environment
-      DB .......................... /usr/local/share/app/app.db
-      PATH_INFO ...................
-      REMOTE_USER .................
-      SCRIPT_EXEC ................. /usr/local/bin/wapptclsh /home/quixote/projects/hello.tcl
-      SCRIPT_FILENAME ............. /usr/local/bin/wapptclsh
-      SCRIPT_NAME ................. /wapp/hello
-      SECRET ...................... /usr/local/share/app/secret
-    Inherited environment
-      HOME ........................ /home/quixote
-      LANG ........................ en_US.UTF-8
-    Placeholders
-      {path} ...................... /hello
-      {root} ...................... /
-      {http.request.host} ......... example.com
-      {http.request.host} ......... GET
-      {http.request.host} ......... /wapp/hello.tcl
+	Executable .................... /usr/local/bin/wapptclsh
+	  Arg 1 ....................... /home/quixote/projects/hello.tcl
+	Root .......................... /
+	Dir ........................... /home/quixote/www
+	Environment
+	  DB .......................... /usr/local/share/app/app.db
+	  PATH_INFO ...................
+	  REMOTE_USER .................
+	  SCRIPT_EXEC ................. /usr/local/bin/wapptclsh /home/quixote/projects/hello.tcl
+	  SCRIPT_FILENAME ............. /usr/local/bin/wapptclsh
+	  SCRIPT_NAME ................. /wapp/hello
+	  SECRET ...................... /usr/local/share/app/secret
+	Inherited environment
+	  HOME ........................ /home/quixote
+	  LANG ........................ en_US.UTF-8
+	Placeholders
+	  {path} ...................... /hello
+	  {root} ...................... /
+	  {http.request.host} ......... example.com
+	  {http.request.host} ......... GET
+	  {http.request.host} ......... /wapp/hello.tcl
 
 This information can be used to diagnose problems with how a CGI
 application is called.
@@ -262,19 +267,19 @@ application is called.
 To return to operation mode, remove or comment out the inspect
 subdirective.
 
-Environment Variable Example
+# Environment Variable Example
 
 In this example, the Caddyfile looks like this:
 
-    {
-        http_port 8080
-        order cgi last
-    }
+	{
+	    http_port 8080
+	    order cgi last
+	}
 
-    192.168.1.2:8080
-    cgi /show* /usr/local/cgi-bin/report/gen {
-        script_name /show
-    }
+	192.168.1.2:8080
+	cgi /show* /usr/local/cgi-bin/report/gen {
+	    script_name /show
+	}
 
 Note that a request for /show gets mapped to a script named
 /usr/local/cgi-bin/report/gen. There is no need for any element of the
@@ -282,37 +287,37 @@ script name to match any element of the match pattern.
 
 The contents of /usr/local/cgi-bin/report/gen are:
 
-    #!/bin/bash
+	#!/bin/bash
 
-    printf "Content-type: text/plain\n\n"
+	printf "Content-type: text/plain\n\n"
 
-    printf "example error message\n" > /dev/stderr
+	printf "example error message\n" > /dev/stderr
 
-    if [ "POST" = "$REQUEST_METHOD" -a -n "$CONTENT_LENGTH" ]; then
-      read -n "$CONTENT_LENGTH" POST_DATA
-    fi
+	if [ "POST" = "$REQUEST_METHOD" -a -n "$CONTENT_LENGTH" ]; then
+	  read -n "$CONTENT_LENGTH" POST_DATA
+	fi
 
-    printf "AUTH_TYPE         [%s]\n" $AUTH_TYPE
-    printf "CONTENT_LENGTH    [%s]\n" $CONTENT_LENGTH
-    printf "CONTENT_TYPE      [%s]\n" $CONTENT_TYPE
-    printf "GATEWAY_INTERFACE [%s]\n" $GATEWAY_INTERFACE
-    printf "PATH_INFO         [%s]\n" $PATH_INFO
-    printf "PATH_TRANSLATED   [%s]\n" $PATH_TRANSLATED
-    printf "POST_DATA         [%s]\n" $POST_DATA
-    printf "QUERY_STRING      [%s]\n" $QUERY_STRING
-    printf "REMOTE_ADDR       [%s]\n" $REMOTE_ADDR
-    printf "REMOTE_HOST       [%s]\n" $REMOTE_HOST
-    printf "REMOTE_IDENT      [%s]\n" $REMOTE_IDENT
-    printf "REMOTE_USER       [%s]\n" $REMOTE_USER
-    printf "REQUEST_METHOD    [%s]\n" $REQUEST_METHOD
-    printf "SCRIPT_EXEC       [%s]\n" $SCRIPT_EXEC
-    printf "SCRIPT_NAME       [%s]\n" $SCRIPT_NAME
-    printf "SERVER_NAME       [%s]\n" $SERVER_NAME
-    printf "SERVER_PORT       [%s]\n" $SERVER_PORT
-    printf "SERVER_PROTOCOL   [%s]\n" $SERVER_PROTOCOL
-    printf "SERVER_SOFTWARE   [%s]\n" $SERVER_SOFTWARE
+	printf "AUTH_TYPE         [%s]\n" $AUTH_TYPE
+	printf "CONTENT_LENGTH    [%s]\n" $CONTENT_LENGTH
+	printf "CONTENT_TYPE      [%s]\n" $CONTENT_TYPE
+	printf "GATEWAY_INTERFACE [%s]\n" $GATEWAY_INTERFACE
+	printf "PATH_INFO         [%s]\n" $PATH_INFO
+	printf "PATH_TRANSLATED   [%s]\n" $PATH_TRANSLATED
+	printf "POST_DATA         [%s]\n" $POST_DATA
+	printf "QUERY_STRING      [%s]\n" $QUERY_STRING
+	printf "REMOTE_ADDR       [%s]\n" $REMOTE_ADDR
+	printf "REMOTE_HOST       [%s]\n" $REMOTE_HOST
+	printf "REMOTE_IDENT      [%s]\n" $REMOTE_IDENT
+	printf "REMOTE_USER       [%s]\n" $REMOTE_USER
+	printf "REQUEST_METHOD    [%s]\n" $REQUEST_METHOD
+	printf "SCRIPT_EXEC       [%s]\n" $SCRIPT_EXEC
+	printf "SCRIPT_NAME       [%s]\n" $SCRIPT_NAME
+	printf "SERVER_NAME       [%s]\n" $SERVER_NAME
+	printf "SERVER_PORT       [%s]\n" $SERVER_PORT
+	printf "SERVER_PROTOCOL   [%s]\n" $SERVER_PROTOCOL
+	printf "SERVER_SOFTWARE   [%s]\n" $SERVER_SOFTWARE
 
-    exit 0
+	exit 0
 
 The purpose of this script is to show how request information gets
 communicated to a CGI script. Note that POST data must be read from
@@ -324,70 +329,70 @@ including all arguments, with which the CGI script was executed.
 
 When a browser requests
 
-        http://192.168.1.2:8080/show/weekly?mode=summary
+	http://192.168.1.2:8080/show/weekly?mode=summary
 
 the response looks like
 
-    AUTH_TYPE         []
-    CONTENT_LENGTH    []
-    CONTENT_TYPE      []
-    GATEWAY_INTERFACE [CGI/1.1]
-    PATH_INFO         [/weekly]
-    PATH_TRANSLATED   []
-    POST_DATA         []
-    QUERY_STRING      [mode=summary]
-    REMOTE_ADDR       [192.168.1.35]
-    REMOTE_HOST       [192.168.1.35]
-    REMOTE_IDENT      []
-    REMOTE_USER       []
-    REQUEST_METHOD    [GET]
-    SCRIPT_EXEC       [/usr/local/cgi-bin/report/gen]
-    SCRIPT_NAME       [/show]
-    SERVER_NAME       [192.168.1.2:8080]
-    SERVER_PORT       [8080]
-    SERVER_PROTOCOL   [HTTP/1.1]
-    SERVER_SOFTWARE   [go]
+	AUTH_TYPE         []
+	CONTENT_LENGTH    []
+	CONTENT_TYPE      []
+	GATEWAY_INTERFACE [CGI/1.1]
+	PATH_INFO         [/weekly]
+	PATH_TRANSLATED   []
+	POST_DATA         []
+	QUERY_STRING      [mode=summary]
+	REMOTE_ADDR       [192.168.1.35]
+	REMOTE_HOST       [192.168.1.35]
+	REMOTE_IDENT      []
+	REMOTE_USER       []
+	REQUEST_METHOD    [GET]
+	SCRIPT_EXEC       [/usr/local/cgi-bin/report/gen]
+	SCRIPT_NAME       [/show]
+	SERVER_NAME       [192.168.1.2:8080]
+	SERVER_PORT       [8080]
+	SERVER_PROTOCOL   [HTTP/1.1]
+	SERVER_SOFTWARE   [go]
 
 When a client makes a POST request, such as with the following command
 
-    wget -O - -q --post-data="city=San%20Francisco" http://192.168.1.2:8080/show/weekly?mode=summary
+	wget -O - -q --post-data="city=San%20Francisco" http://192.168.1.2:8080/show/weekly?mode=summary
 
 the response looks the same except for the following lines:
 
-    CONTENT_LENGTH    [20]
-    CONTENT_TYPE      [application/x-www-form-urlencoded]
-    POST_DATA         [city=San%20Francisco]
-    REQUEST_METHOD    [POST]
+	CONTENT_LENGTH    [20]
+	CONTENT_TYPE      [application/x-www-form-urlencoded]
+	POST_DATA         [city=San%20Francisco]
+	REQUEST_METHOD    [POST]
 
-Go Source Example
+# Go Source Example
 
 This small example demonstrates how to write a CGI program in Go. The
 use of a bytes.Buffer makes it easy to report the content length in the
 CGI header.
 
-    package main
+	package main
 
-    import (
-        "bytes"
-        "fmt"
-        "os"
-        "time"
-    )
+	import (
+	    "bytes"
+	    "fmt"
+	    "os"
+	    "time"
+	)
 
-    func main() {
-        var buf bytes.Buffer
+	func main() {
+	    var buf bytes.Buffer
 
-        fmt.Fprintf(&buf, "Server time at %s is %s\n",
-            os.Getenv("SERVER_NAME"), time.Now().Format(time.RFC1123))
-        fmt.Println("Content-type: text/plain")
-        fmt.Printf("Content-Length: %d\n\n", buf.Len())
-        buf.WriteTo(os.Stdout)
-    }
+	    fmt.Fprintf(&buf, "Server time at %s is %s\n",
+	        os.Getenv("SERVER_NAME"), time.Now().Format(time.RFC1123))
+	    fmt.Println("Content-type: text/plain")
+	    fmt.Printf("Content-Length: %d\n\n", buf.Len())
+	    buf.WriteTo(os.Stdout)
+	}
 
 When this program is compiled and installed as
 /usr/local/bin/servertime, the following directive in your Caddy file
 will make it available:
 
-    cgi /servertime /usr/local/bin/servertime
+	cgi /servertime /usr/local/bin/servertime
 */
 package cgi
