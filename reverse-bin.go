037f6b3c8830c5ed8b6abeed0d1126e6dff1896e
Taras Glek <taras@glek.net>
2026-01-25 17:41:01 +0200

    refactor: remove inspect.go and rename cgi files to reverse-bin

---
+++ reverse-bin.go
@@ -0,0 +1,263 @@
+/*
+ * Copyright (c) 2017 Kurt Jung (Gmail: kurt.w.jung)
+ * Copyright (c) 2020 Andreas Schneider
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+package reversebin
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"os/exec"
+	"runtime"
+	"strings"
+	"sync"
+	"syscall"
+	"time"
+
+	"github.com/caddyserver/caddy/v2"
+	"github.com/caddyserver/caddy/v2/modules/caddyhttp"
+	"go.uber.org/zap"
+)
+
+// passAll returns a slice of strings made up of each environment key
+func passAll() (list []string) {
+	envList := os.Environ() // ["HOME=/home/foo", "LVL=2", ...]
+	for _, str := range envList {
+		pos := strings.Index(str, "=")
+		if pos > 0 {
+			list = append(list, str[:pos])
+		}
+	}
+	return
+}
+
+func (c *ReverseBin) ServeHTTP(w http.ResponseWriter, r *http.Request, next caddyhttp.Handler) error {
+	return c.serveProxy(w, r, next)
+}
+
+func (c *ReverseBin) killProcessGroup() {
+	if c.process == nil {
+		return
+	}
+	if runtime.GOOS != "windows" {
+		// Kill the process group
+		syscall.Kill(-c.process.Pid, syscall.SIGKILL)
+	} else {
+		c.process.Kill()
+	}
+}
+
+type instantWriter struct {
+	http.ResponseWriter
+}
+
+func (iw instantWriter) Write(b []byte) (int, error) {
+	n, err := iw.ResponseWriter.Write(b)
+	iw.ResponseWriter.(http.Flusher).Flush()
+	return n, err
+}
+
+func (c *ReverseBin) serveProxy(w http.ResponseWriter, r *http.Request, next caddyhttp.Handler) error {
+	c.mu.Lock()
+	if c.process == nil {
+		if err := c.startProcess(); err != nil {
+			c.mu.Unlock()
+			return err
+		}
+	}
+
+	// Stop idle timer if running
+	if c.idleTimer != nil {
+		c.idleTimer.Stop()
+		c.idleTimer = nil
+	}
+
+	c.activeRequests++
+	c.logger.Debug("incremented active requests", zap.Int64("count", c.activeRequests))
+	c.mu.Unlock()
+
+	defer func() {
+		c.mu.Lock()
+		defer c.mu.Unlock()
+
+		c.activeRequests--
+		c.logger.Debug("decremented active requests", zap.Int64("count", c.activeRequests))
+		if c.activeRequests == 0 {
+			c.idleTimer = time.AfterFunc(30*time.Second, func() {
+				c.mu.Lock()
+				defer c.mu.Unlock()
+				if c.activeRequests == 0 && c.process != nil {
+					c.terminationMsg = "idle timeout"
+					c.killProcessGroup()
+					c.process = nil
+				}
+			})
+		}
+	}()
+
+	if c.reverseProxy == nil {
+		return fmt.Errorf("reverse proxy not initialized")
+	}
+
+	return c.reverseProxy.ServeHTTP(w, r, next)
+}
+
+func (c *ReverseBin) startProcess() error {
+	cmd := exec.Command(c.Executable, c.Args...)
+	if runtime.GOOS != "windows" {
+		cmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
+	}
+	cmd.Dir = c.WorkingDirectory
+	if cmd.Dir == "" {
+		cmd.Dir = "."
+	}
+
+	var cmdEnv []string
+	if c.PassAll {
+		cmdEnv = os.Environ()
+	} else {
+		for _, key := range c.PassEnvs {
+			if val, ok := os.LookupEnv(key); ok {
+				cmdEnv = append(cmdEnv, key+"="+val)
+			}
+		}
+	}
+	cmdEnv = append(cmdEnv, c.Envs...)
+	cmd.Env = cmdEnv
+
+	stdoutPipe, err := cmd.StdoutPipe()
+	if err != nil {
+		return err
+	}
+	stderrPipe, err := cmd.StderrPipe()
+	if err != nil {
+		return err
+	}
+
+	c.logger.Info("starting proxy subprocess",
+		zap.String("executable", cmd.Path),
+		zap.Strings("args", cmd.Args))
+
+	if err := cmd.Start(); err != nil {
+		c.logger.Error("failed to start proxy subprocess",
+			zap.String("executable", cmd.Path),
+			zap.Error(err))
+		return err
+	}
+	c.process = cmd.Process
+
+	exitChan := make(chan error, 1)
+	go func() {
+		var wg sync.WaitGroup
+		drain := func(name string, pipe io.ReadCloser) {
+			defer wg.Done()
+			reader := bufio.NewReader(pipe)
+			for {
+				line, err := reader.ReadString('\n')
+				if len(line) > 0 {
+					c.logger.Info("reverse proxy process "+name, zap.String("msg", strings.TrimSuffix(line, "\n")))
+				}
+				if err != nil {
+					break
+				}
+			}
+		}
+
+		wg.Add(2)
+		go drain("stdout", stdoutPipe)
+		go drain("stderr", stderrPipe)
+
+		wg.Wait()
+		err := cmd.Wait()
+
+		c.mu.Lock()
+		reason := c.terminationMsg
+		if reason == "" {
+			reason = "unexpected exit"
+		}
+		c.terminationMsg = ""
+		if c.process == cmd.Process {
+			c.process = nil
+		}
+		c.mu.Unlock()
+
+		c.logger.Info("proxy subprocess terminated",
+			zap.String("executable", cmd.Path),
+			zap.String("reason", reason),
+			zap.Error(err))
+		exitChan <- err
+	}()
+
+	// Readiness check
+	expected := c.ReverseProxyTo
+	if strings.HasPrefix(expected, ":") {
+		expected = "127.0.0.1" + expected
+	}
+	expected = strings.TrimPrefix(expected, "http://")
+	expected = strings.TrimPrefix(expected, "https://")
+
+	readyChan := make(chan bool, 1)
+	if c.ReadinessMethod != "" {
+		scheme := "http"
+		if strings.HasPrefix(c.ReverseProxyTo, "https://") {
+			scheme = "https"
+		}
+		checkURL := fmt.Sprintf("%s://%s%s", scheme, expected, c.ReadinessPath)
+		c.logger.Info("waiting for reverse proxy process readiness via HTTP polling",
+			zap.String("method", c.ReadinessMethod),
+			zap.String("url", checkURL))
+
+		go func() {
+			client := &http.Client{Timeout: 500 * time.Millisecond}
+			ticker := time.NewTicker(200 * time.Millisecond)
+			defer ticker.Stop()
+			for {
+				select {
+				case <-ticker.C:
+					req, _ := http.NewRequest(c.ReadinessMethod, checkURL, nil)
+					resp, err := client.Do(req)
+					if err == nil {
+						resp.Body.Close()
+						if resp.StatusCode >= 200 && resp.StatusCode < 400 {
+							readyChan <- true
+							return
+						}
+					}
+				case <-c.ctx.Done():
+					return
+				}
+			}
+		}()
+	} else {
+		// If no HTTP check, we assume it's ready immediately as we are draining stdout
+		// (The previous stdout-substring logic was fragile and blocked the drainer)
+		readyChan <- true
+	}
+
+	select {
+	case <-readyChan:
+		c.logger.Info("reverse proxy process ready", zap.String("address", expected))
+		return nil
+	case err := <-exitChan:
+		return fmt.Errorf("reverse proxy process exited during readiness check: %v", err)
+	case <-time.After(10 * time.Second):
+		c.killProcessGroup()
+		return fmt.Errorf("timeout waiting for reverse proxy process readiness")
+	}
+}
---
+++ reverse-bin_test.go
@@ -0,0 +1,204 @@
+package reversebin
+
+import (
+	"context"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"reflect"
+	"strconv"
+	"strings"
+	"testing"
+
+	"github.com/caddyserver/caddy/v2"
+	"github.com/caddyserver/caddy/v2/caddyconfig/caddyfile"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zaptest"
+)
+
+func TestReverseBin_UnmarshalCaddyfile(t *testing.T) {
+	content := `reverse-bin /some/file a b c d 1 {
+  dir /somewhere
+  env foo=bar what=ever
+  pass_env some_env other_env
+  pass_all_env
+}`
+	d := caddyfile.NewTestDispenser(content)
+	var c ReverseBin
+	if err := c.UnmarshalCaddyfile(d); err != nil {
+		t.Fatalf("Cannot parse caddyfile: %v", err)
+	}
+
+	expected := ReverseBin{
+		Executable:       "/some/file",
+		WorkingDirectory: "/somewhere",
+		Args:             []string{"a", "b", "c", "d", "1"},
+		Envs:             []string{"foo=bar", "what=ever"},
+		PassEnvs:         []string{"some_env", "other_env"},
+		PassAll:          true,
+	}
+
+	if !reflect.DeepEqual(c, expected) {
+		t.Fatal("Parsing yielded invalid result.")
+	}
+}
+
+type NoOpNextHandler struct{}
+
+func (n NoOpNextHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) error {
+	// Do Nothing
+	return nil
+}
+
+func _TestReverseBin_ServeHTTPPost(t *testing.T) {
+	testSetup := []struct {
+		name         string
+		uri          string
+		method       string
+		requestBody  string
+		responseBody string
+		cgi          ReverseBin
+		statusCode   int
+		chunked      bool
+	}{
+		{
+			name: "POST Request",
+			cgi: ReverseBin{
+				Executable: "test/example_post",
+				ScriptName: "/foo.cgi",
+				Args:       []string{"arg1", "arg2"},
+				Envs:       []string{"CGI_GLOBAL=whatever"},
+				logger:     zaptest.NewLogger(t, zaptest.Level(zap.ErrorLevel)),
+			},
+			uri:    "foo.cgi/some/path?x=y",
+			method: http.MethodPost,
+			requestBody: `Chunked HTTP Request Body
+With some awesome stuff in there like
+this and that and also
+this and that and also
+this and that and also
+this and that and also
+this and that and also`,
+			statusCode: 200,
+			responseBody: `PATH_INFO [/some/path]
+CGI_GLOBAL [whatever]
+Arg 1 [arg1]
+QUERY_STRING [x=y]
+REMOTE_USER []
+HTTP_TOKEN_CLAIM_USER []
+CGI_LOCAL is unset
+Chunked HTTP Request Body
+With some awesome stuff in there like
+this and that and also
+this and that and also
+this and that and also
+this and that and also
+this and that and also`,
+		},
+		{
+			name: "POST Request with chunked Transfer-Encoding In-Memory",
+			cgi: ReverseBin{
+				Executable:  "test/example_post",
+				ScriptName:  "/foo.cgi",
+				Args:        []string{"arg1", "arg2"},
+				Envs:        []string{"CGI_GLOBAL=whatever"},
+				logger:      zaptest.NewLogger(t, zaptest.Level(zap.ErrorLevel)),
+				BufferLimit: 200,
+			},
+			uri:    "foo.cgi/some/path?x=y",
+			method: http.MethodPost,
+			requestBody: `Chunked HTTP Request Body
+With some awesome stuff in there like
+this and that and also
+this and that and also
+this and that and also
+this and that and also
+this and that and also`,
+			statusCode: 200,
+			responseBody: `PATH_INFO [/some/path]
+CGI_GLOBAL [whatever]
+Arg 1 [arg1]
+QUERY_STRING [x=y]
+REMOTE_USER []
+HTTP_TOKEN_CLAIM_USER []
+CGI_LOCAL is unset
+Chunked HTTP Request Body
+With some awesome stuff in there like
+this and that and also
+this and that and also
+this and that and also
+this and that and also
+this and that and also`,
+			chunked: true,
+		},
+		{
+			name: "POST Request with chunked Transfer-Encoding tempfile",
+			cgi: ReverseBin{
+				Executable:  "test/example_post",
+				ScriptName:  "/foo.cgi",
+				Args:        []string{"arg1", "arg2"},
+				Envs:        []string{"CGI_GLOBAL=whatever"},
+				logger:      zaptest.NewLogger(t, zaptest.Level(zap.ErrorLevel)),
+				BufferLimit: 100,
+			},
+			uri:    "foo.cgi/some/path?x=y",
+			method: http.MethodPost,
+			requestBody: `Chunked HTTP Request Body
+With some awesome stuff in there like
+this and that and also
+this and that and also
+this and that and also
+this and that and also
+this and that and also`,
+			statusCode: 200,
+			responseBody: `PATH_INFO [/some/path]
+CGI_GLOBAL [whatever]
+Arg 1 [arg1]
+QUERY_STRING [x=y]
+REMOTE_USER []
+HTTP_TOKEN_CLAIM_USER []
+CGI_LOCAL is unset
+Chunked HTTP Request Body
+With some awesome stuff in there like
+this and that and also
+this and that and also
+this and that and also
+this and that and also
+this and that and also`,
+			chunked: true,
+		},
+	}
+
+	for _, testCase := range testSetup {
+		t.Run(testCase.name, func(t *testing.T) {
+			res := httptest.NewRecorder()
+			req := httptest.NewRequest(http.MethodPost, "/foo.cgi/some/path?x=y", nil)
+
+			if testCase.chunked {
+				req.Header.Set("Transfer-Encoding", "chunked")
+				req.TransferEncoding = []string{"chunked"}
+			} else {
+				cl := len(testCase.requestBody)
+				req.Header.Set("Content-Length", strconv.Itoa(cl))
+				req.ContentLength = int64(cl)
+			}
+			req.Body = io.NopCloser(strings.NewReader(testCase.requestBody))
+
+			repl := caddy.NewReplacer()
+			req = req.WithContext(context.WithValue(req.Context(), caddy.ReplacerCtxKey, repl))
+
+			if err := testCase.cgi.ServeHTTP(res, req, NoOpNextHandler{}); err != nil {
+				t.Fatalf("Cannot serve http: %v", err)
+			}
+
+			if res.Code != testCase.statusCode {
+				t.Errorf("Unexpected statusCode %d. Expected %d.", res.Code, testCase.statusCode)
+			}
+
+			bodyString := strings.TrimSpace(res.Body.String())
+			if bodyString != testCase.responseBody {
+				t.Errorf("Unexpected body\n========== Got ==========\n%s\n========== Wanted ==========\n%s", bodyString, testCase.responseBody)
+			}
+		})
+	}
+}
