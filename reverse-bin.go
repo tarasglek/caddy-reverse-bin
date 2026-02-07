6649fda6f96099a3f610553089c0049ca067caa7
Taras Glek <taras@glek.net>
2026-02-07 16:01:48 +0200

    misc

--- reverse-bin.go
+++ reverse-bin.go
@@ -1,6 +1,7 @@
 /*
  * Copyright (c) 2017 Kurt Jung (Gmail: kurt.w.jung)
  * Copyright (c) 2020 Andreas Schneider
+ * Copyright (c) 2025 Taras Glek
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
