a6841b56e28db7aa0eab8158494280197bffa8a2
Taras Glek <taras@glek.net>
2026-02-14 17:25:52 +0200

    Rename project/module to reverse-bin (#2)
    * docs(module): rename project to caddy-reverse-bin
    
    * fix(module): switch module path to v1 import

--- cmd/caddy/main.go
+++ cmd/caddy/main.go
@@ -33,7 +33,7 @@ import (
 
 	// plug in Caddy modules here
 	_ "github.com/caddyserver/caddy/v2/modules/standard"
-	_ "github.com/tarasglek/caddy-reverseweb/v2"
+	_ "github.com/tarasglek/reverse-bin"
 )
 
 func main() {
