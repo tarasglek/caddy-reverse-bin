838ad45e6c1456c84a41da451fe206ffcf42990d
Taras Glek <taras@glek.net>
2026-06-19 15:07:26 +0300

    refactor: remove hosting files from plugin repo

--- examples/reverse-bin-apps-config/run.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-echo "deprecated: use systemd with /usr/bin/reverse-bin-caddy and /etc/reverse-bin/Caddyfile" >&2
-exit 1
--- examples/reverse-bin-apps-config/setup-systemd.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-import sys
-
-print("deprecated: install the Debian package and manage reverse-bin.service directly", file=sys.stderr)
-raise SystemExit(1)
