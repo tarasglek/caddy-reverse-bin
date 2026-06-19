aeed3c467d4b60be82532f27e02405559b21ab02
Taras Glek <taras@glek.net>
2026-06-19 15:57:44 +0300

    test: use go echo app for plugin integration

--- examples/reverse-proxy/run.sh
+++ examples/reverse-proxy/run.sh
@@ -7,5 +7,8 @@ pushd $CONFIG_DIR/../..
 
 air="go run github.com/air-verse/air@v1.64.4"
 
+# Build the proof-of-concept backend used by the example Caddyfile.
+go build -o "$CONFIG_DIR/apps/go-echo/go-echo" "$CONFIG_DIR/apps/go-echo"
+
 # Call the central run script
-$air --build.entrypoint "./tmp/caddy"  --build.cmd "go build -o ./tmp/caddy cmd/caddy/main.go" -build.include_ext "go,config,json" --build.include_file /home/taras/Documents/caddy-reverse-bin/examples/reverse-proxy/Caddyfile -build.args_bin "run --adapter caddyfile --config $CONFIG_DIR/Caddyfile" -d
+$air --build.entrypoint "./tmp/caddy"  --build.cmd "go build -o ./tmp/caddy cmd/caddy/main.go" -build.include_ext "go,config,json" --build.include_file "$CONFIG_DIR/Caddyfile" -build.args_bin "run --adapter caddyfile --config $CONFIG_DIR/Caddyfile" -d
