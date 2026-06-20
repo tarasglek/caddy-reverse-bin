9de2655356cc94a023dc56d712c1a3f9ed6b185e
Taras Glek <taras@glek.net>
2026-06-20 10:57:48 +0300

    chore(examples): remove air runner

--- examples/reverse-proxy/run.sh
+++ examples/reverse-proxy/run.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 set -ex
 
-# Change to the directory of this script
+# Change to the directory of this script.
 export CONFIG_DIR=$(realpath $(dirname "$0"))
-pushd $CONFIG_DIR/../..
-
-air="go run github.com/air-verse/air@v1.64.4"
+pushd "$CONFIG_DIR/../.."
 
 # Build the example backend and detector spawned by reverse-bin.
 go build -o "$CONFIG_DIR/apps/go-echo/go-echo" "$CONFIG_DIR/apps/go-echo"
 go build -o "$CONFIG_DIR/detector/example-detector" "$CONFIG_DIR/detector"
 
-# Call the central run script. The example Caddyfile also spawns ./tmp/caddy as
-# a child static file server to demonstrate reverse-bin managing a Caddy subprocess.
-$air --build.entrypoint "./tmp/caddy"  --build.cmd "go build -o ./tmp/caddy cmd/caddy/main.go" -build.include_ext "go,config,json" --build.include_file "$CONFIG_DIR/Caddyfile" -build.args_bin "run --adapter caddyfile --config $CONFIG_DIR/Caddyfile" -d
+# Build and run the Caddy binary with this plugin. The example Caddyfile also
+# spawns ./tmp/caddy as a child static file server to demonstrate reverse-bin
+# managing a Caddy subprocess.
+go build -o ./tmp/caddy ./cmd/caddy
+exec ./tmp/caddy run --adapter caddyfile --config "$CONFIG_DIR/Caddyfile"
